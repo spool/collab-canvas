132635dbd4dfdc871ff2c4ccb2119212b28f12ff
Dr Griffith Rees <griff.rees@gmail.com>
2019-03-03 23:36:25 +0000

    Add in VisualCellEditForm experiment in forms.py, likely to be removed.

--- collab_canvas/visual/forms.py
+++ collab_canvas/visual/forms.py
@@ -1,14 +1,21 @@
 from django.forms import ModelForm
-from .models import VisualCanvas
+from .models import VisualCellEdit
 
 
-class VisualCanvasForm(ModelForm):
+class VisualCellEditForm(ModelForm):
 
     """Form for editing a cell on a canvas."""
 
     class Meta:
 
-        model = VisualCanvas
+        model = VisualCellEdit
+        fields = VisualCellEdit.get_edge_names()
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.artist = kwargs['artist']
+
+    # def __init__(self, cell_id, artist_id,)
 
     # def clean_x_position(self, *args, **kwargs):
     #     """
